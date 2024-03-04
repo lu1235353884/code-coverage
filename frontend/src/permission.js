808e2db11893e955b0514a4d9648d6f5fbd64594
lujunb <lujunb@digiwin.com>
2024-03-04 14:34:45 +0800

    前端代码提交

--- frontend/src/permission.js
+++ frontend/src/permission.js
@@ -1,11 +1,11 @@
 import router, { resetRouter } from './router'
 import store from './store'
-import storage from 'store'
+// import storage from 'store'
 import NProgress from 'nprogress' // progress bar
 import '@/components/NProgress/nprogress.less' // progress bar custom style
 import notification from 'ant-design-vue/es/notification'
 import { setDocumentTitle, domTitle } from '@/utils/domUtil'
-import { ACCESS_TOKEN } from '@/store/mutation-types'
+// import { ACCESS_TOKEN } from '@/store/mutation-types'
 import { i18nRender } from '@/locales'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
@@ -18,7 +18,8 @@ router.beforeEach((to, from, next) => {
   NProgress.start() // start progress bar
   to.meta && typeof to.meta.title !== 'undefined' && setDocumentTitle(`${i18nRender(to.meta.title)} - ${domTitle}`)
   /* has token */
-  const token = storage.get(ACCESS_TOKEN)
+  // const token = storage.get(ACCESS_TOKEN)
+  const token = '4291d7da9005377ec9aec4a71ea837f'
   if (token) {
     if (to.path === loginRoutePath) {
       next({ path: defaultRoutePath })
@@ -71,8 +72,6 @@ router.beforeEach((to, from, next) => {
     } else {
       next({ path: loginRoutePath, query: { redirect: to.fullPath } })
       NProgress.done() // if current page is login will not trigger afterEach hook, so manually handle it
-      // 暂时改成都免登录
-      // next()
     }
   }
 })
