fbcd1d41a4000dbaa5391c06d8dd3362934d1f5e
xiarui <x442333964@163.com>
2024-03-11 16:25:11 +0800

    提交

---
+++ backend/src/main/java/com/digiwin/code/coverage/backend/pojo/po/AppBranchPO.java
@@ -0,0 +1,30 @@
+package com.digiwin.code.coverage.backend.pojo.po;
+
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@TableName(value = "app_branch")
+public class AppBranchPO {
+
+  @TableId(type = IdType.AUTO)
+  private Long id;
+  private Long relId;
+  private String appCode;
+  private String sourceBranchName;
+  private String targetBranchName;
+  private String allFilePath;
+  private Date allFileDate;
+  private String diffFilePath;
+  private Date diffFileDate;
+  private Date downloadBranchDate;
+  private Date downloadDataFileDate;
+  private Date compileDate;
+  private Date compareDate;
+  
+}
